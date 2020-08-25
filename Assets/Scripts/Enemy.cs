eda753d1ddb4d5240146ff6fd8aed4231dfa54dc
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-08-25 17:53:12 -0500

    Allow the enemy to create their own box colliders since they are prefabbed from the asset pack
    Allow the enemy to create their own box colliders since they are prefabbed from the asset pack

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        BoxCollider enemyBoxCollider = gameObject.AddComponent<BoxCollider>() as BoxCollider;
+        enemyBoxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        Destroy(gameObject);
+    }
+}
