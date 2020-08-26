bed006689f5b53b7e1e4f09bbb70b53dc692a78d
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-08-25 21:16:12 -0500

    Add score into UI Canvas
    Add score into UI Canvas

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 120;
+
+    int score = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        UpdateScore();
+    }
+
+    public void ScoreHit()
+    {
+        score += scorePerHit;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = score.ToString();
+    }
+}
